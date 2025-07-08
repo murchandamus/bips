bc7dc8ca35aa8218b876886bce9f675d7244e42d
Yuval Kogman <nothingmuch@woobling.org>
2025-07-08 14:49:40 +0200

    Allow `Version` field in checks as per BIP 3

--- scripts/buildtable.pl
+++ scripts/buildtable.pl
@@ -39,6 +39,9 @@ my %MiscField = (
 	'Requires' => undef,
 	'Proposed-Replacement' => undef,
 );
+my %VersionField = (
+	'Version' => undef,
+);
 
 my %ValidLayer = (
 	'Consensus (soft fork)' => undef,
@@ -173,6 +176,8 @@ while (++$bipnum <= $topbip) {
 			die "Invalid date format in $fn" unless $val =~ /^20\d{2}\-(?:0\d|1[012])\-(?:[012]\d|30|31)$/;
 		} elsif (exists $EmailField{$field}) {
 			$val =~ m/^(\S[^<@>]*\S) \<[^@>]*\@[\w.]+\.\w+\>$/ or die "Malformed $field line in $fn";
+		} elsif (exists $VersionField{$field}) {
+			$val =~ m/^(\d+\.\d+\.\d+)$/ or die "Malformed $field line in $fn";
 		} elsif (not exists $MiscField{$field}) {
 			die "Unknown field $field in $fn";
 		}
